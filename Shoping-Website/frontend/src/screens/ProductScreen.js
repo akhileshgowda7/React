97582ff54997756957ee5966d4e03458b27568f5
akhileshgowda <akhileshgowda7@gmail.com>
2022-11-03 14:46:37 -0400

    Product screen completion

---
+++ Shoping-Website/frontend/src/screens/ProductScreen.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Card,
+  Button,
+  ListGroupItem,
+} from 'react-bootstrap';
+import Rating from '../components/Rating';
+import products from '../products';
+import { useParams } from 'react-router-dom';
+
+const ProductScreen = () => {
+  const params = useParams();
+  const product = products.find((p) => p._id === params.id);
+  return (
+    <>
+      <Link className="btn btn-dark my-3" to="/">
+        Browse All Products
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid></Image>
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroupItem>
+              <h3>{product.name}</h3>
+            </ListGroupItem>
+            <ListGroupItem>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+              ></Rating>
+            </ListGroupItem>
+            <ListGroupItem>Price: ${product.price}</ListGroupItem>
+            <ListGroupItem>Description: {product.description}</ListGroupItem>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroupItem>
+                <Row>
+                  <Col>Price</Col>
+                  <Col>
+                    <strong>{product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroupItem>
+              <ListGroupItem>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
+                  </Col>
+                </Row>
+              </ListGroupItem>
+              <ListGroupItem>
+                <Button
+                  className="btn Block"
+                  type="button"
+                  disabled={product.countInStock === 0}
+                >
+                  Add to Cart
+                </Button>
+              </ListGroupItem>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default ProductScreen;

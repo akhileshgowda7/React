f5257d1677ab3c3ddb392117716fdefe9de8f468
akhileshgowda <akhileshgowda7@gmail.com>
2022-11-06 15:02:38 -0500

    Seerver file edit for db

--- Shoping-Website/backend/server.js
+++ Shoping-Website/backend/server.js
@@ -1,5 +1,12 @@
-const express = require('express');
-const products = require('./data/products');
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import colors from 'colors'
+import products from './data/products.js';
+
+dotenv.config();
+
+connectDB();
 
 const app = express(); //initializing express
 
@@ -12,8 +19,12 @@ app.get('/api/products', (req, res) => {
 });
 
 app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
+  const product = find((p) => p._id === req.params.id);
   res.json(product);
 });
 
-app.listen(5001, console.log('Server running on port 50001'));
+const PORT = process.env.PORT || 5001;
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+);

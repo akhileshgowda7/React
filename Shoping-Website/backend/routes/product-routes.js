0d37df6426b1d5e149517f2250686652821036d3
akhileshgowda <akhileshgowda7@gmail.com>
2022-11-09 12:26:39 -0500

    Changed error handling method

--- Shoping-Website/backend/routes/product-routes.js
+++ Shoping-Website/backend/routes/product-routes.js
@@ -1,10 +1,8 @@
 import express from 'express';
 import Product from '../models/productModel.js';
 import asyncHandler from 'express-async-handler';
-import { useParams } from 'react-router-dom';
 
 const router = express.Router();
-const params = useParams();
 
 //@desc  Fetch all products
 //@route GET /api/products
@@ -12,8 +10,8 @@ const params = useParams();
 router.get(
   '/',
   asyncHandler(async (req, res) => {
-    const product = await Product.find({}); //empty object gives everything
-    res.json();
+    const products = await Product.find({}); //empty object gives everything
+    res.json(products);
   })
 );
 
@@ -24,12 +22,13 @@ router.get(
 router.get(
   '/:id',
   asyncHandler(async (req, res) => {
-    const product = await Product.findById(params.id);
+    const product = await Product.findById(req.params.id);
 
     if (product) {
       res.json(product);
     } else {
-      res.status(404).json({ message: 'Product not found' });
+      res.status(404);
+      throw new Error('Product not Found');
     }
   })
 );

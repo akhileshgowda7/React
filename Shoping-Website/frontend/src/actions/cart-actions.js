b56d1066ef53dde16ebf1167b2daf254ac1e4be8
akhileshgowda <akhileshgowda7@gmail.com>
2022-11-26 19:44:56 -0500

    adding items to cart screen

--- Shoping-Website/frontend/src/actions/cart-actions.js
+++ Shoping-Website/frontend/src/actions/cart-actions.js
@@ -9,11 +9,12 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
     payload: {
       product: data._id,
       name: data.name,
-      image: data.price,
+      image: data.image,
+      price:data.price,
       countInStock: data.countInStock,
       qty,
     },
   });
 
-  localStorage.setItem('cartItem',JSON.stringify(getState().cart.cartItems));
+  localStorage.setItem('cartItem', JSON.stringify(getState().cart.cartItems));
 };
--- Shoping-Website/frontend/src/screens/CartScreen.js
+++ Shoping-Website/frontend/src/screens/CartScreen.js
@@ -11,6 +11,8 @@ import {
   Form,
   Button,
   Card,
+  ListGroupItem,
+  FormControl,
 } from 'react-bootstrap';
 import Message from '../components/Message';
 import { addToCart } from '../actions/cart-actions';
@@ -34,6 +36,10 @@ const CartScreen = () => {
     }
   }, [dispatch, productid, qty]);
 
+  const removeFromCartHandler = (id) => {
+    console.log('remove');
+  };
+
   return (
     <Row>
       <Col md={8}>
@@ -43,11 +49,59 @@ const CartScreen = () => {
             Your cart is empty <Link to="/">Go Back</Link>
           </Message>
         ) : (
-          <ListGroup variant="flush"></ListGroup>
+          <ListGroup variant="flush">
+            {cartItems.map((item) => (
+              <ListGroupItem key={item.product}>
+                <Row>
+                  <Col md={2}>
+                    <Image
+                      src={item.image}
+                      alt={item.name}
+                      fluid
+                      rounded
+                    ></Image>
+                  </Col>
+                  <Col md={3}>
+                    <Link to={`/product/${item.product}`}>{item.name}</Link>
+                  </Col>
+                  <Col md={2}>${item.price}</Col>
+                  <Col md={2}>
+                    <FormControl
+                      as="select"
+                      value={item.qty}
+                      onChange={(e) =>
+                        dispatch(
+                          addToCart(item.product, Number(e.target.value))
+                        )
+                      }
+                    >
+                      {[...Array(item.countInStock).keys()].map((x) => (
+                        <option key={x + 1} value={x + 1}>
+                          {x + 1}
+                        </option>
+                      ))}
+                    </FormControl>
+                  </Col>
+                  <Col md={2}>
+                    {' '}
+                    <Button
+                      type="button"
+                      variant="ligth"
+                      onClick={() => removeFromCartHandler(item.product)}
+                    >
+                      <i className="fa fa-trash"></i>
+                    </Button>
+                  </Col>
+                </Row>
+              </ListGroupItem>
+            ))}
+          </ListGroup>
         )}
       </Col>
-      <Col md={2}></Col>
-      <Col md={2}></Col>
+      <Col md={4}>
+        
+      </Col>
+      
     </Row>
   );
 };

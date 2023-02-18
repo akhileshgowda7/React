de8d1d60bbd533eeafab0f022b84521b6e3b154d
akhileshgowda <akhileshgowda7@gmail.com>
2023-02-18 16:42:27 -0500

    added user login reducer,constant and actions

--- Shoping-Website/backend/controllers/user-controller.js
+++ Shoping-Website/backend/controllers/user-controller.js
@@ -23,6 +23,38 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
+//@desc Register a new user
+//@route POST /api/users
+//@access PUBLIC
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User Already Exists');
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(404);
+    throw new Error('Invalid User data');
+  }
+});
+
 //@desc GET User Profilr
 //@route GET /api/users/profile
 //@access PRIVATE
@@ -42,4 +74,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
   res.send('Success');
 });
 
-export { authUser, getUserProfile };
+export { authUser, getUserProfile, registerUser };
--- Shoping-Website/frontend/src/store.js
+++ Shoping-Website/frontend/src/store.js
@@ -5,19 +5,27 @@ import {
   productDetailsReducer,
 } from './reducers/product-reducers';
 import { cartReducer } from './reducers/cart-reducer.js';
-
+import { userLoginReducer } from './reducers/user-reducers';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 });
 const cartItemsFromStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
   : [];
 
-const initialState = { cart: { cartItems: cartItemsFromStorage } };
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null;
+
+const initialState = {
+  cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userInfoFromStorage },
+};
 const middleware = [thunk];
 const store = createStore(
   reducer,

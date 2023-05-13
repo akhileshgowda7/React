f0b673b3d1b3d1931fa4b4c9c95e53e8fa004fb1
akhileshgowda <akhileshgowda7@gmail.com>
2023-05-13 13:49:17 -0400

    listing orders in profile

--- Shoping-Website/frontend/src/actions/order-actions.js
+++ Shoping-Website/frontend/src/actions/order-actions.js
@@ -9,6 +9,9 @@ import {
   ORDER_PAY_FAIL,
   ORDER_PAY_REQUEST,
   ORDER_PAY_SUCCESS,
+  ORDER_LIST_MY_REQUEST,
+  ORDER_LIST_MY_SUCCESS,
+  ORDER_LIST_MY_FAIL,
 } from '../constants/order-constants';
 
 export const createOrder = (order) => async (dispatch, getState) => {
@@ -46,7 +49,7 @@ export const createOrder = (order) => async (dispatch, getState) => {
 };
 
 export const getOrderDetails = (id) => async (dispatch, getState) => {
-  console.log('orderId',id)
+  console.log('orderId', id);
   try {
     dispatch({
       type: ORDER_DETAILS_REQUEST,
@@ -81,7 +84,7 @@ export const getOrderDetails = (id) => async (dispatch, getState) => {
 
 export const payOrder =
   (orderId, paymentResult) => async (dispatch, getState) => {
-    console.log(orderId)
+    console.log(orderId);
     try {
       dispatch({
         type: ORDER_PAY_REQUEST,
@@ -118,3 +121,36 @@ export const payOrder =
       });
     }
   };
+
+export const listmyOrders = () => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: ORDER_LIST_MY_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.get(`/api/orders/myorders`, config);
+
+    dispatch({
+      type: ORDER_LIST_MY_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: ORDER_LIST_MY_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
--- Shoping-Website/frontend/src/screens/ProfileScreen.js
+++ Shoping-Website/frontend/src/screens/ProfileScreen.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Form, Button, Row, Col } from 'react-bootstrap';
+import { Form, Button, Row, Col, Table } from 'react-bootstrap';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
 import { useDispatch, useSelector } from 'react-redux';
 import { getUserDetails, updateUserProfile } from '../actions/user-actions';
+import { listmyOrders } from '../actions/order-actions';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const ProfileScreen = () => {
   const [email, setEmail] = useState('');
@@ -24,6 +26,10 @@ const ProfileScreen = () => {
   const userUpdateProfile = useSelector((state) => state.userUpdateProfile);
   const { success } = userUpdateProfile;
 
+  const orderListMy = useSelector((state) => state.orderListMy);
+  const { loading: loadingOrders, error: errorOrders, orders } = orderListMy;
+  console.log(orders);
+
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -32,6 +38,7 @@ const ProfileScreen = () => {
     } else {
       if (!user.name) {
         dispatch(getUserDetails('profile'));
+        dispatch(listmyOrders());
       } else {
         setName(user.name);
         setEmail(user.email);
@@ -114,6 +121,56 @@ const ProfileScreen = () => {
       </Col>
       <Col md={9}>
         <h2>My Orders</h2>
+        {loadingOrders ? (
+          <Loader />
+        ) : errorOrders ? (
+          <Message variant="danger">{errorOrders}</Message>
+        ) : (
+          // ...
+
+          <Table striped bordered hover responsive className="table-sm">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>DATE</th>
+                <th>TOTAL</th>
+                <th>PAID</th>
+                <th>DELIVERED</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {orders.map((order) => (
+                <tr key={order._id}>
+                  <td>{order._id}</td>
+                  <td>{order.createdAt && order.createdAt.substring(0, 10)}</td>
+                  <td>{order.totalPrice}</td>
+                  <td>
+                    {order.isPaid ? (
+                      order.paidAt && order.paidAt.substring(0, 10)
+                    ) : (
+                      <i className="fas fa-times" style={{ color: 'red' }}></i>
+                    )}
+                  </td>
+                  <td>
+                    {order.isDelivered ? (
+                      order.deliveredAt && order.deliveredAt.substring(0, 10)
+                    ) : (
+                      <i className="fas fa-times" style={{ color: 'red' }}></i>
+                    )}
+                  </td>
+                  <td>
+                    <LinkContainer to={`/order/${order._id}`}>
+                      <Button className="btn-sm" variant="light">
+                        Details
+                      </Button>
+                    </LinkContainer>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        )}
       </Col>
     </Row>
   );

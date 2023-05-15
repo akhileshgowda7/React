2df25d30c7336e4fae3291efba63a304024ea455
akhileshgowda <akhileshgowda7@gmail.com>
2023-05-15 18:04:20 -0400

    delete product controller

--- Shoping-Website/backend/controllers/product-controller.js
+++ Shoping-Website/backend/controllers/product-controller.js
@@ -4,7 +4,6 @@ import asyncHandler from 'express-async-handler';
 const getProducts = asyncHandler(async (req, res) => {
   const products = await Product.find({}); //empty object gives everything
   res.json(products);
-  
 });
 
 const getProductById = asyncHandler(async (req, res) => {
@@ -18,4 +17,83 @@ const getProductById = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProducts, getProductById };
+/**
+ * @desc DELETE a product
+ * @route DELETE /api/products/:id
+ * @access Private/ADMIN
+ */
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    await product.remove();
+    res.json({ message: 'Product removed' });
+  } else {
+    res.status(404);
+    throw new Error('Product not Found');
+  }
+});
+
+// @desc    Update a product
+// @route   PUT /api/products/:id
+// @access  Private/Admin
+const updateProduct = asyncHandler(async (req, res) => {
+  const {
+    name,
+    price,
+    description,
+    image,
+    brand,
+    category,
+    countInStock,
+  } = req.body
+
+  const product = await Product.findById(req.params.id)
+
+  if (product) {
+    product.name = name
+    product.price = price
+    product.description = description
+    product.image = image
+    product.brand = brand
+    product.category = category
+    product.countInStock = countInStock
+
+    const updatedProduct = await product.save()
+    res.json(updatedProduct)
+  } else {
+    res.status(404)
+    throw new Error('Product not found')
+  }
+})
+
+/**
+ * @desc POST a product
+ * @route POST /api/products/:id
+ * @access Private/ADMIN
+ */
+
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: 'Sample name',
+    price: 0,
+    user: req.user._id,
+    image: '/images/sample.jpg',
+    brand: 'Sample brand',
+    category: 'Sample category',
+    countInStock: 0,
+    numReviews: 0,
+    description: 'Sample description',
+  })
+
+  const createdProduct = await product.save()
+  res.status(201).json(createdProduct)
+})
+
+export {
+  getProducts,
+  getProductById,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+};

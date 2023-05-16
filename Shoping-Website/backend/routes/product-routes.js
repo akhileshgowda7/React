60b457de3f0013bd547dc78996848a68e687d5d4
akhileshgowda <akhileshgowda7@gmail.com>
2023-05-16 17:38:58 -0400

    adding product routws

--- Shoping-Website/backend/routes/product-routes.js
+++ Shoping-Website/backend/routes/product-routes.js
@@ -1,14 +1,21 @@
 import express from 'express';
 import {
+  createProduct,
+  deleteProduct,
   getProductById,
   getProducts,
+  updateProduct,
 } from '../controllers/product-controller.js';
+import { protect, admin } from '../middleware/auth-middleware.js';
 
 const router = express.Router();
 
-router.route('/').get(getProducts);
+router.route('/').get(getProducts).post(protect, admin, createProduct);
 
-router.route('/:id').get(getProductById);
+router
+  .route('/:id')
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct);
 
 export default router;
- 

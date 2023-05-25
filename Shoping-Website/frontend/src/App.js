de93a3cf68226e9eca8f3ec9c7b379cfa7c487f1
akhileshgowda <akhileshgowda7@gmail.com>
2023-05-25 16:10:08 -0400

    Added order list screen for admins

--- Shoping-Website/frontend/src/App.js
+++ Shoping-Website/frontend/src/App.js
@@ -17,6 +17,7 @@ import UserListScreen from './screens/UserListScreen';
 import UserEditScreen from './screens/UserEditScreen';
 import ProductListScreen from './screens/ProductListScreen';
 import ProductEditScreen from './screens/ProductEditScreen';
+import OrderListScreen from './screens/OrderListScreen';
 
 const App = () => {
   return (
@@ -35,10 +36,15 @@ const App = () => {
             <Route path="/product/:id" element={<ProductScreen />} />
             <Route path="/cart/" element={<CartScreen />} />
             <Route path="/cart/:id" element={<CartScreen />} />
-            <Route path="/admin/userlist" element={<UserListScreen/>} />
-            <Route path="/admin/productlist" element={<ProductListScreen/>} />
-            <Route path="/admin/user/:id/edit" element={<UserEditScreen/>} />
-            <Route path="/admin/product/:id/edit" element={<ProductEditScreen/>} />
+            <Route path="/admin/userlist" element={<UserListScreen />} />
+            <Route path="/admin/productlist" element={<ProductListScreen />} />
+            <Route path="/admin/user/:id/edit" element={<UserEditScreen />} />
+            <Route path="/admin/orderlist" element={<OrderListScreen />} />
+
+            <Route
+              path="/admin/product/:id/edit"
+              element={<ProductEditScreen />}
+            />
 
             <Route path="/" element={<HomeScreen />} exact />
           </Routes>

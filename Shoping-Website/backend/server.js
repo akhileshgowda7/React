af4988102d1ec85077750c1e7441a21d527e9070
akhileshgowda <akhileshgowda7@gmail.com>
2023-05-25 15:37:27 -0400

    added product upload functionality

--- Shoping-Website/backend/server.js
+++ Shoping-Website/backend/server.js
@@ -5,8 +5,9 @@ import colors from 'colors';
 import productRoutes from './routes/product-routes.js';
 import userRoutes from './routes/user-routes.js';
 import { notFound, errorHandler } from './middleware/error-middleware.js';
-import orderRoutes from './routes/order-routes.js'
-
+import orderRoutes from './routes/order-routes.js';
+import uploadRoutes from './routes/upload-routes.js';
+import path from 'path';
 dotenv.config();
 
 connectDB();
@@ -21,8 +22,13 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
+app.get('/api/config/paypal', (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+);
 
-app.get('/api/config/paypal',(req,res)=>res.send(process.env.PAYPAL_CLIENT_ID ))
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.use(notFound);
 
--- Shoping-Website/frontend/src/screens/ProductEditScreen.js
+++ Shoping-Website/frontend/src/screens/ProductEditScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useLocation, useNavigate, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import { Form, Button } from 'react-bootstrap';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
@@ -7,6 +7,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { listProductDetails, updateProduct } from '../actions/product-actions';
 import FormContainer from '../components/FormContainer';
 import { PRODUCT_UPDATE_RESET } from '../constants/product-constants';
+import axios from 'axios';
 
 const ProductEditScreen = () => {
   const params = useParams();
@@ -20,6 +21,7 @@ const ProductEditScreen = () => {
   const [category, setCategory] = useState('');
   const [countInStock, setCountInStock] = useState(0);
   const [description, setDescription] = useState('');
+  const [uploading, setUploading] = useState(false);
 
   const dispatch = useDispatch();
 
@@ -52,6 +54,29 @@ const ProductEditScreen = () => {
       }
     }
   }, [dispatch, product, productId]);
+
+  const uploadFileHandler = async (e) => {
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append('image', file);
+    setUploading(true);
+
+    try {
+      const config = {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      };
+
+      const { data } = await axios.post('/api/upload', formData, config);
+
+      setImage(data);
+      setUploading(false);
+    } catch (error) {
+      console.error(error);
+      setUploading(false);
+    }
+  };
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -118,6 +143,16 @@ const ProductEditScreen = () => {
                   setImage(e.target.value);
                 }}
               ></Form.Control>
+
+              <Form.Control
+                controlId="image-file"
+                label="Choose File"
+                type="file"
+                custom
+                onChange={uploadFileHandler}
+              ></Form.Control>
+
+              {uploading && <Loader />}
             </Form.Group>
 
             <Form.Group controlId="brand">
